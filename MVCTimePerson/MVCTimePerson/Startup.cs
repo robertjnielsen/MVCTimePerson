d4fcd63ab7894ba2eb294cab19b771599f8d4f79
Robert James Nielsen <robert.j.nielsen@outlook.com>
2020-03-31 11:32:11 -0700

    Startup.cs modified to include MVC routing and static files. Home controller and relating Index view created. Proof of life successful.

--- MVCTimePerson/MVCTimePerson/Startup.cs
+++ MVCTimePerson/MVCTimePerson/Startup.cs
@@ -16,6 +16,7 @@ namespace MVCTimePerson
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddMvc();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -26,14 +27,15 @@ namespace MVCTimePerson
                 app.UseDeveloperExceptionPage();
             }
 
+            app.UseStaticFiles();
             app.UseRouting();
 
             app.UseEndpoints(endpoints =>
             {
-                endpoints.MapGet("/", async context =>
-                {
-                    await context.Response.WriteAsync("Start w/o Debug, quick refresh!");
-                });
+                endpoints.MapControllerRoute(
+                    name: "default",
+                    pattern: "{controller=Home}/{action=Index}/{id?}"
+                );
             });
         }
     }
